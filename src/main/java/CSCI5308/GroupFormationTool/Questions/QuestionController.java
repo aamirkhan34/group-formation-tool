969377698c55af2497f5c28bf9493908eb313b27
Prem Kumar <pr775390@dal.ca>
2020-06-16 15:21:26 -0300

    Added table columns for more answers

--- src/main/java/CSCI5308/GroupFormationTool/Questions/QuestionController.java
+++ src/main/java/CSCI5308/GroupFormationTool/Questions/QuestionController.java
@@ -1,30 +1,30 @@
 package CSCI5308.GroupFormationTool.Questions;
 
-import CSCI5308.GroupFormationTool.Courses.Course;
-import CSCI5308.GroupFormationTool.Courses.ICoursePersistence;
-import CSCI5308.GroupFormationTool.SystemConfig;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class QuestionController {
 
-    @GetMapping("/question/questionmanager")
+    @RequestMapping("/question/questionmanager")
     public String createQuestion(Model model)
     {
         model.addAttribute("question", new Question());
-        return "question/questionmanager";
+        return "/question/questionmanager";
     }
 
-    @GetMapping("/question/questionanswer")
-    public String createAnswer(Model model){
-        Question question = new Question();
-        return "question/questionanswer";
+    @RequestMapping( value = "/question/questionanswer", method = RequestMethod.POST)
+    public String createAnswer(Model model, Question question, RedirectAttributes attr){
+        attr.addFlashAttribute("question", question);
+        return "redirect:/question/questionoptions";
+    }
+
+    @RequestMapping(value = "/question/questionoptions", method = RequestMethod.GET)
+    public String displayQuestion(Question question){
+        return "/question/questionanswer";
     }
 
 }
