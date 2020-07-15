a426213232a5018dcb573fa5bf3bb89052dca41d
basapure <pr676280@dal.ca>
2020-07-15 18:52:02 -0300

    Displaying only survey questions for group formation

--- SQL/spLoadSurveyQuestionsByCourseId.sql
+++ SQL/spLoadSurveyQuestionsByCourseId.sql
@@ -7,8 +7,10 @@ CREATE PROCEDURE spLoadSurveyQuestionsByCourseId (
 IN id BIGINT
 )
 BEGIN
-	select SurveyQuestions.questionID,Survey.isPublished from Survey
+	select SurveyQuestions.questionID,QuestionType.questionTypeName,Question.title,Question.questionText,Question.createdOn,Survey.isPublished from Survey
 join SurveyQuestions on Survey.id=SurveyQuestions.surveyID
+join Question on SurveyQuestions.questionID=Question.id
+join QuestionType on Question.type_id=QuestionType.id
     WHERE Survey.courseID = id;
 END $$
 
