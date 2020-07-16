e33f2e80141ed5b7460954a4ded2316f6b310718
asakhan <am754815@dal.ca>
2020-07-16 16:35:30 -0300

    updated create Group DB method

--- src/main/java/CSCI5308/GroupFormationTool/GroupFormation/GroupDB.java
+++ src/main/java/CSCI5308/GroupFormationTool/GroupFormation/GroupDB.java
@@ -2,6 +2,7 @@ package CSCI5308.GroupFormationTool.GroupFormation;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -15,7 +16,35 @@ public class GroupDB implements IGroupPersistence {
 	@Override
 	public boolean createGroups(List<Group> group) {
 		CallStoredProcedure proc = null;
-		return false;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			for (Group g : group) {
+				proc = new CallStoredProcedure("spCreateGroup(?, ?, ?, ?)");
+				proc.setParameter(1, g.getCourse().getId());
+				proc.setParameter(2, sdf.format(g.getCreatedOn()));
+				proc.setParameter(3, sdf.format(g.getCreatedOn()));
+				proc.registerOutputParameterLong(4);
+				proc.execute();
+				
+				Long groupID = proc.getLongParameters(4);
+				proc = new CallStoredProcedure("spCreateGroupMembers(?, ?, ?)");
+				for (int i = 0; i < g.getStudents().size(); i++) {
+					proc.setParameter(1, groupID);
+					proc.setParameter(2, g.getStudents().get(i).getId());
+					proc.registerOutputParameterLong(3);
+					proc.execute();
+				}
+			}
+		} catch (SQLException e) {
+			// Logging needed
+			System.out.println(e);
+			return false;
+		} finally {
+			if (null != proc) {
+				proc.cleanup();
+			}
+		}
+		return true;
 	}
 
 	@Override
@@ -24,25 +53,23 @@ public class GroupDB implements IGroupPersistence {
 		List<Group> groups = new ArrayList<>();
 		try {
 			proc = new CallStoredProcedure("spGroupIDByCourseID(?)");
-			proc.setParameter(1,course.getId());
+			proc.setParameter(1, course.getId());
 			ResultSet resultSet = proc.executeWithResults();
-			while (resultSet.next()){
+			while (resultSet.next()) {
 				ArrayList<User> users = new ArrayList<>();
 				int groupID = resultSet.getInt(1);
 				Date createOn = resultSet.getDate(2);
 				proc = new CallStoredProcedure("spUserIDByGroupID(?)");
-				proc.setParameter(1,groupID);
+				proc.setParameter(1, groupID);
 				ResultSet resultSet1 = proc.executeWithResults();
-				while (resultSet1.next()){
+				while (resultSet1.next()) {
 					User user = new User();
 					int eachUserID = resultSet1.getInt(1);
 					proc = new CallStoredProcedure("spLoadUser(?)");
 					proc.setParameter(1, eachUserID);
 					ResultSet results = proc.executeWithResults();
-					if (null != results)
-					{
-						while (results.next())
-						{
+					if (null != results) {
+						while (results.next()) {
 							long userID = results.getLong(1);
 							String bannerID = results.getString(2);
 							String firstName = results.getString(4);
@@ -57,15 +84,13 @@ public class GroupDB implements IGroupPersistence {
 						users.add(user);
 					}
 				}
-				Group group = new Group(groupID,course,createOn,users);
+				Group group = new Group(groupID, course, createOn, users);
 				groups.add(group);
 			}
 			return groups;
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			if (null != proc) {
 				proc.cleanup();
 			}
